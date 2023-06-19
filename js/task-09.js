256d7de4a0ca0e13da2da6302565a38c70697edd
Zelenro <125198729+Zelenro@users.noreply.github.com>
2023-06-19 20:31:42 +0300

    HW-1-DOM-Event

--- js/task-09.js
+++ js/task-09.js
@@ -8,12 +8,19 @@ const bodyColorEl = document.querySelector('.widget');
 const nameColorEl = document.querySelector('.color');
 const changeColorButtonEl = document.querySelector('.change-color');
 
-changeColorButtonEl.addEventListener('click', () => {
+changeColorButtonEl.addEventListener('click', btnClick);
+function btnClick() {
   const randomColor = getRandomHexColor();
   bodyColorEl.style.backgroundColor = randomColor;
   nameColorEl.textContent = randomColor;
-});
+}
 
 // Напиши скрипт, який змінює кольори фону елемента < body > через інлайн -
 // стиль по кліку на button.change - color і виводить значення кольору в span.color.
 // Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+// changeColorButtonEl.addEventListener('click', () => {
+//   const randomColor = getRandomHexColor();
+//   bodyColorEl.style.backgroundColor = randomColor;
+//   nameColorEl.textContent = randomColor;
+// });
